870e38c591632aa9587ee1657b8cb656716a0452
Jorge Coronado <60112119+JorgeACoronado@users.noreply.github.com>
2026-02-14 13:07:26 -0600

    Clean up

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
-<script setup>
-import NavMenu from '@/components/NavMenu.vue'
-</script>
 
 <template>
   <div class="flex min-h-screen flex-col">
-    <NavMenu />
     <RouterView />
   </div>
 </template>
